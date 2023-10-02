e492caf51ed24111f588ddc88d80ffc64001a0b6
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-10-02 18:25:13 +0530

    Add files via upload

---
+++ LinkedList/deleteAlternateElement.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+class LinkedList{
+    public:
+    Node* head;
+    LinkedList(){
+        head = NULL;
+    }
+    void insertAtTail(int value){
+        Node* new_node = new Node(value);
+        if(head==NULL){
+            //linked list is empty
+            head = new_node;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+    void display(){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->val<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+    }
+};
+void deleteAlternateElement(Node* &head){
+    Node* curr = head;
+    while(curr!=NULL && curr->next!=NULL){
+        Node* temp = curr->next; //this is the node to be deleted
+        curr->next = curr->next->next;
+        free(temp);
+        curr = curr->next;
+    }
+}
+
+int main(){
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.display();
+    deleteAlternateElement(ll.head);
+    ll.display();
+    return 0;
+}
